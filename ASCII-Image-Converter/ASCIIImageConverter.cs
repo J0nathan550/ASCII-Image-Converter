91718d6910d76c01f8be27e35457f90962232fd4
J0nathan550 <ihudokormov@gmail.com>
2022-08-06 23:28:04 +0300

    Add project files.

---
+++ ASCII-Image-Converter/ASCIIImageConverter.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+
+namespace J0nathan550.ASCIIImageCreator
+{
+    public class ASCIIImageConverter
+    {
+        private string imagePath;
+
+        public ASCIIImageConverter(string imagePath)
+        {
+            this.imagePath = imagePath;
+        }
+
+        private string pixels = " .-+*wGHM#&$%";
+
+        public void Compile(string saveImagePath)
+        {
+            var image = new Bitmap(imagePath);
+            using(var writer = new StreamWriter(saveImagePath))
+            {
+                for (var y = 0; y < image.Height; y++)
+                {
+                    for (var x = 0; x < image.Width; x++)
+                    {
+                        var color = image.GetPixel(x, y);
+                        var brightness = Brightness(color);
+                        var index = brightness / 255 * (pixels.Length - 1);
+                        var pixel = pixels[(int)Math.Round(index)];
+                        writer.Write(pixel);
+                        writer.Write(pixel);
+                    }
+                    writer.WriteLine();
+                }
+                    writer.Close();
+                    Console.WriteLine(File.ReadAllText(saveImagePath));
+            }
+        } 
+        private double Brightness(Color col)
+        {
+            return Math.Sqrt(col.R * col.R * .246 + col.G * col.G * .691 + col.B * col.B * .068);
+        }
+    }
+}
---
+++ ASCII-Image-Converter/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using J0nathan550.ASCIIImageCreator;
+public class Program
+{
+
+    public static void Main(string[] args)
+    {
+        while (true)
+        {
+            try
+            {
+                Console.Write("Welcome to ASCII image creator! write a path to image:");
+                string imagePath = Console.ReadLine();
+                Console.WriteLine();
+                Console.Write("Write the output of file:");
+                string savePath = Console.ReadLine();
+                ASCIIImageConverter imageConverter = new ASCIIImageConverter(imagePath);
+                Console.WriteLine("Done! Here is the result:");
+                imageConverter.Compile(savePath);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);   
+            }
+        }
+
+    }
+}
